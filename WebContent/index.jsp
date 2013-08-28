0bf299c3373dc9ab697ad591678d41925cd549d0
Max <martin.vyrostko@appstract.dk>
2013-08-28 11:01:24 +0200

    first commit

---
+++ WebContent/index.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=US-ASCII"
+    pageEncoding="US-ASCII"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=US-ASCII">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1>Email Validation</h1>
+	<form action="MyServlet" method="get">
+		<input type="text" name="email" placeholder="insert email" /><input type="submit" value="send" />
+	</form>
+</body>
+</html>
---
+++ src/com/src/pkg/MyServlet.java
@@ -0,0 +1,48 @@
+package com.src.pkg;
+
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String[] arr;
+		PrintWriter out = response.getWriter();
+		arr = request.getParameterValues("email");
+		if(arr == null || arr[0].isEmpty()) {
+			out.print("You have to fill in the email.");
+		} else if(arr[0].contains("@")) {
+			out.print("Email is valid: " + arr[0]);			
+		} else {
+			out.print("Email is invalid: " + arr[0]);
+		}
+		request.getSession().setAttribute("hello", "hello2");
+		out.println();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
